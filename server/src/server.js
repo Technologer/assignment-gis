c377caf19d86c406fca37a50648e076cd2aa617f
Jan Vnencak <jv80ss@gmail.com>
2018-11-23 18:36:18 +0100

    first scenario added

--- server/src/server.js
+++ server/src/server.js
@@ -8,15 +8,29 @@ const PORT = 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// to enable CORS
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
+
 app.post('/api/pdt', (req, res) => {
+  console.log(req.body)
   db.query(
-    'SELECT * FROM test',
+    // 'SELECT ST_AsGeoJSON(geom) from accidents LIMIT 100',
+    // 'SELECT latitude, longitude from accidents LIMIT 100',
+    'SELECT st_asgeojson(geom) FROM accidents CROSS JOIN (SELECT st_setsrid(st_point(-0.12810230255126956, 51.54968430132381), 4326) as point) as test WHERE st_distance(geom::geography, test.point::geography) < 100',
     [],
     (err, data) => {
       if (err) {
         res.send(err);
       } else {
-        res.send(data.rows);
+        res.send(data.rows.map(item => JSON.parse(item.st_asgeojson)));
+        // res.send(data.rows);
       }
     }
   );
